29fb6c91d19baa5b2bc01ec71a5ffcaf6dbf960e
Glitch (coordinated-jury) <none>
2018-03-20 17:19:14 +0000

    cleaning things up!

--- server.js
+++ server.js
@@ -40,22 +40,6 @@ app.use("/", (req,res) =>{
   res.end(query);
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
